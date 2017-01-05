85082f6db2c3ee250062909f98142417c18fe257
FredericoVieira <fredops1@hotmail.com>
2017-01-05 20:15:49 -0200

    Updates files to deploy on heroku.

--- manage.py
+++ manage.py
@@ -1,7 +1,11 @@
 from app import manager
+from flask_script import Server
 from flask_migrate import MigrateCommand
 from scripts.load_data import LoadData
+from os import getenv
+
 
 manager.add_command('loaddata', LoadData)
 manager.add_command('db', MigrateCommand)
+manager.add_command('runserver', Server(host='0.0.0.0', port=int(getenv('PORT', 5000))))
 manager.run()
